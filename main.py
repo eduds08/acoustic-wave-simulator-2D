8040a4ff5e05490ab4d7e859021506b23a68a779
eduds08 <edudscoelho@outlook.com>
2023-10-05 23:05:41 -0300

    replaced loops for vectorized operation and gif

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib as mpl
+from scipy.ndimage import laplace
+from PIL import Image
 
-
-def laplacian(x, z, dx, P):
-    # dz = dx
-    return (P[x + 1, z] + P[x - 1, z] + P[x, z + 1] + P[x, z - 1] - 4 * P[x, z]) / (dx ** 2)
-
+# def laplacian(x, z, dx, P):
+#     return (P[x + 1, z] + P[x - 1, z] + P[x, z + 1] + P[x, z - 1] - 4 * P[x, z]) / (dx ** 2)
 
 x_grid_size = 100
 z_grid_size = x_grid_size
@@ -42,15 +40,33 @@ c = np.zeros((x_grid_size, z_grid_size))
 c += c0
 
 for t in range(total_time_steps):
-    for x in range(1, x_grid_size - 1):
-        for z in range(1, z_grid_size - 1):
-            p_future[x, z] = (c[x, z] ** 2) * laplacian(x, z, delta_x, p_present) * \
-                (delta_time ** 2) + 2 * p_present[x, z] - p_past[x, z]
+
+    p_future = (c ** 2) * laplace(p_present) * (delta_time ** 2)
+
+    p_future += 2 * p_present - p_past
+
+    # for x in range(1, x_grid_size - 1):
+    #     for z in range(1, z_grid_size - 1):
+
+    #         p_future[x, z] = (c[x, z] ** 2) * laplacian(x, z, delta_x, p_present) * \
+    #             (delta_time ** 2) + 2 * p_present[x, z] - p_past[x, z]
 
     p_past = p_present
     p_present = p_future
 
     p_future[source_x, source_z] += source[t]
-    print(t)
 
     plt.imsave(f"imagens/image_filename{t}.png", p_future)
+
+frames = []
+imgs = []
+for t in range(total_time_steps):
+    imgs.append(f"imagens/image_filename{t}.png")
+for i in imgs:
+    new_frame = Image.open(i)
+    frames.append(new_frame)
+
+frames[0].save('simulation.gif', format='GIF',
+               append_images=frames[1:],
+               save_all=True,
+               duration=50, loop=0)
