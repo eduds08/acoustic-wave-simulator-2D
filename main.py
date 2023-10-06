fe241c94859f004030a54b5c02ea209102a68fe6
eduds08 <edudscoelho@outlook.com>
2023-10-05 22:33:35 -0300

    save each plot of p_future in a .png

--- main.py
+++ main.py
@@ -1,34 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib as mpl
-from matplotlib.animation import PillowWriter
 
 
-# dz = dx (change it later to work on a not regular grid!)
 def laplacian(x, z, dx, P):
+    # dz = dx
     return (P[x + 1, z] + P[x - 1, z] + P[x, z + 1] + P[x, z - 1] - 4 * P[x, z]) / (dx ** 2)
 
-writer = PillowWriter(fps=15)
 
-x_grid_size = 200
+x_grid_size = 100
 z_grid_size = x_grid_size
-delta_x = .5
+delta_x = 1
 delta_z = delta_x
 
-total_time_steps = 1000
+total_time_steps = 502
 delta_time = 0.0010
 
 source_x = int(x_grid_size / 2)
 source_z = int(z_grid_size / 2)
 
-c0 = 250.
+c0 = 340
 
 epsilon = c0 * delta_time / delta_x
 
 print(f'CFL Epsilon: {epsilon}\n')
 
-f0 = 500
-t0 = 1. / f0
+f0 = 25
+t0 = 2 / f0
 
 source = np.zeros(total_time_steps + 1)
 time = np.linspace(0, total_time_steps * delta_time, total_time_steps)
@@ -43,12 +41,9 @@ p_future = np.zeros((x_grid_size, z_grid_size))
 c = np.zeros((x_grid_size, z_grid_size))
 c += c0
 
-fig = plt.figure()
-writer.setup(fig, 'teste.gif', 100)
-
 for t in range(total_time_steps):
-    for x in range(1, x_grid_size - 1):  # 1 - 498
-        for z in range(1, z_grid_size - 1):  # 1 - 498
+    for x in range(1, x_grid_size - 1):
+        for z in range(1, z_grid_size - 1):
             p_future[x, z] = (c[x, z] ** 2) * laplacian(x, z, delta_x, p_present) * \
                 (delta_time ** 2) + 2 * p_present[x, z] - p_past[x, z]
 
@@ -56,9 +51,6 @@ for t in range(total_time_steps):
     p_present = p_future
 
     p_future[source_x, source_z] += source[t]
-    
-    plt.plot(p_future)
+    print(t)
 
-    writer.grab_frame()
-
-writer.finish()
+    plt.imsave(f"imagens/image_filename{t}.png", p_future)
