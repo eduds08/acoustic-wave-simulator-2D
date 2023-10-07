18d32aa91dbedc472eaba9c183bf3d11c3ebd142
eduds08 <edudscoelho@outlook.com>
2023-10-07 19:29:11 -0300

    laplace for 5 operator done

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.ndimage import laplace
 from PIL import Image
-
-import glob
 import cv2
 
-# def laplacian(x, z, dx, P):
-#     return (P[x + 1, z] + P[x - 1, z] + P[x, z + 1] + P[x, z - 1] - 4 * P[x, z]) / (dx ** 2)
+
+def laplacian(x, z, dx, P):
+    return (P[x + 1, z] + P[x - 1, z] + P[x, z + 1] + P[x, z - 1] - 4 * P[x, z]) / (dx ** 2)
+
+
+def laplacian_5_operator(z, x, dx, P):
+    return (-P[z + 2, x] + 16 * P[z + 1, x] - 60 * P[z, x] + 16 * P[z - 1, x] - P[z - 2, x] - P[z, x + 2] + 16 * P[z, x + 1] + 16 * P[z, x - 1] - P[z, x - 2]) / (12 * dx ** 2)
 
 
 def create_gif(total_time):
@@ -42,7 +45,7 @@ z_grid_size = x_grid_size
 delta_x = 1
 delta_z = delta_x
 
-total_time_steps = 1500
+total_time_steps = 7500
 delta_time = 0.001
 
 source_x = int(x_grid_size / 2)
@@ -73,10 +76,27 @@ p_future = np.zeros((z_grid_size, x_grid_size))
 c = np.zeros((z_grid_size, x_grid_size))
 c += c0
 
+pxx = np.zeros((z_grid_size, x_grid_size))
+pzz = np.zeros((z_grid_size, x_grid_size))
+
 for t in range(total_time_steps):
 
-    p_future = (c ** 2) * laplace(p_present) * (delta_time ** 2)
+    # for z in range(1, z_grid_size - 1):
+    #     pzz[z, :] = (p_present[z + 1, :] - 2 * p_present[z, :] +
+    #                  p_present[z - 1, :]) / (delta_z ** 2)
+    # for x in range(1, x_grid_size - 1):
+    #     pxx[:, x] = (p_present[:, x + 1] - 2 * p_present[:, x] +
+    #                  p_present[:, x - 1]) / (delta_x ** 2)
 
+    # for z in range(2, z_grid_size - 2):
+    #     pzz[z, :] = ((-1/12) * p_present[z + 2, :] + (4/3) * p_present[z + 1, :] - (5/2) *
+    #                  p_present[z, :] + (4/3) * p_present[z - 1, :] - (1/12) * p_present[z - 2, :]) / (delta_z ** 2)
+    # for x in range(2, x_grid_size - 2):
+    #     pxx[:, x] = ((-1/12) * p_present[:, x + 2] + (4/3) * p_present[:, x + 1] - (5/2) *
+    #                  p_present[:, x] + (4/3) * p_present[:, x - 1] - (1/12) * p_present[:, x - 2]) / (delta_x ** 2)
+
+    p_future = (c ** 2) * laplace(p_present) * (delta_time ** 2)
+    # p_future = (c ** 2) * (pzz + pxx) * (delta_time ** 2)
     p_future += 2 * p_present - p_past
 
     # for x in range(1, x_grid_size - 1):
@@ -108,6 +128,6 @@ for t in range(total_time_steps):
 
     plt.imsave(f"images/plot_{t}.png", p_future)
 
-# create_gif(total_time_steps)
+create_gif(total_time_steps)
 
 create_video(total_time_steps)
